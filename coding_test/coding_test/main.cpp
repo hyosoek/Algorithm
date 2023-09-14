584c979a058a728e83cbeb71db4a5ee66e35f79e
hyosoek <tennfin1@gmail.com>
2023-09-14 19:47:27 +0900

    First commit

---
+++ coding_test/coding_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coding_test
+//
+//  Created by 최효석 on 2023/09/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
